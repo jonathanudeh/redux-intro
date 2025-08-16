d58f6a85316a6c477ec81182027bc0a55e43de85
Jonathan Udeh <jonathanudeh06@gmail.com>
2025-08-16 23:44:44 +0100

    basic redux

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,37 @@
+const inititialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(
+  state = inititialStateCustomer,
+  action
+) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
